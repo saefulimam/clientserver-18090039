062d6f60b70633b0f7fefa0bc8f862af6713b99b
saeful imam <imamsaeful675@gmail.com>
2019-11-18 23:05:31 +0700

    project pertama

---
+++ if_elif_else.py
@@ -0,0 +1,12 @@
+umur = 10
+
+if 0 <= umur <5:
+    print("Umur Masih Balita")
+elif 5<= umur <12:
+    print("Umur Masih Anak-anak")
+elif 12<= umur <19:
+    print ("Umur Masih Remaja")
+elif 19<= umur <60:
+    print("Umur Sudah Dewasa")
+else:
+    print ("Umur Sudah Renta")
---
+++ if_else.py
@@ -0,0 +1,6 @@
+nilai = -9
+
+if nilai >= 0:
+    print ("Nilai Bernilai Positif")
+else:
+    print ("Nilai Bernilai Negatif")
